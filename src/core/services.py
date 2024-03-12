3599b8dbe501e157a9c893a9bb52713b440017e4
Amadeusz Klimaszewski <amadeusz.klimaszewski@gmail.com>
2024-03-12 15:23:02 +0100

    Fix types

--- src/core/services.py
+++ src/core/services.py
@@ -14,10 +14,10 @@ class UrlService:
     def generate_short_url(self) -> uuid.UUID:
         return uuid.uuid4()
 
-    def get_full_shortened_url(self, short_url: uuid.UUID):
+    def get_full_shortened_url(self, short_url: uuid.UUID) -> str:
         return f"http://{settings.HOST}:{settings.PORT}/{short_url}"
 
-    async def create_shortened_url(self, url: str) -> ShortenedUrl:
+    async def create_shortened_url(self, url: str) -> str:
         if url_in_db := await self.repository.get_by_original_url(url):
             return self.get_full_shortened_url(url_in_db.short_url)
 
@@ -26,6 +26,6 @@ class UrlService:
         self.processor.process(shortened_url.short_url, shortened_url.original_url)
         return self.get_full_shortened_url(shortened_url.short_url)
 
-    async def get_original_url(self, shortened_url: uuid.UUID) -> ShortenedUrl:
+    async def get_original_url(self, shortened_url: uuid.UUID) -> str:
         url_in_db = await self.repository.get(pk=shortened_url)
         return url_in_db.original_url
